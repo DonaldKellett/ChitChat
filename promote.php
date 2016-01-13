c41eb54ab8e6dc55c1e667f931015ae06884dc06
DonaldKellett <i.donaldl@me.com>
2016-01-13 13:16:23 +0800

    v1.0.0-dev
    Instruction file not added yet.

---
+++ promote.php
@@ -0,0 +1,116 @@
+<?php
+require 'connect.php';
+require 'vars.php';
+?>
+<!DOCTYPE html>
+<!--
+  ChitChat
+  An easy-to-setup Chat Platform proudly presented to you by DonaldKellett (https://github.com/DonaldKellett)
+  (c) Donald Leung.  All rights reserved.
+  MIT Licensed
+-->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Promote User - ChitChat by DonaldKellett</title>
+    <link rel="stylesheet" href="assets/css/main.css" />
+  </head>
+  <body>
+    <div id="wrapper">
+      <?php require 'header.php'; ?>
+      <div id="main">
+        <?php if ($_COOKIE['logged_in'] == 'true') {
+          $verify_acc = $conn->query("SELECT * FROM accounts WHERE username = " . htmlspecialchars_decode("&quot;") . $_COOKIE['username'] . htmlspecialchars_decode("&quot;") . " AND login_id = " . $_COOKIE['login_id']);
+          if ($verify_acc->num_rows == 1) {
+            while ($my_acc = $verify_acc->fetch_assoc()) {
+              if ($my_acc['rank'] > 2) {
+                $username = $_GET['username'];
+                $get_other_user_by_username = $conn->query("SELECT * FROM accounts WHERE username = " . htmlspecialchars_decode("&quot;") . $username . htmlspecialchars_decode("&quot;") . ";");
+                if ($get_other_user_by_username->num_rows == 1) {
+                  while ($other_acc = $get_other_user_by_username->fetch_assoc()) {
+                    if ($my_acc['rank'] > $other_acc['rank']) {
+                      if ($other_acc['rank'] < 4) {
+                        $new_rank = $other_acc['rank'] + 1;
+                        if ($conn->query("UPDATE accounts SET rank = " . $new_rank . " WHERE username = " . htmlspecialchars_decode("&quot;") . $other_acc['username'] . htmlspecialchars_decode("&quot;") . ";")) { ?>
+                          <h2>Promotion Successful</h2>
+                          <p>
+                            The action was successful and the user selected now has a higher rank.
+                          </p>
+                          <p>
+                            <a href="index.php" class="button special">Return</a>
+                          </p>
+                        <?php } else { ?>
+                          <h2>Action Failed - Server Error</h2>
+                          <p>
+                            Sorry, an error occurred and the action was not successful.  Please send an email to <a href="mailto:dleung@connect.kellettschool.com">dleung@connect.kellettschool.com</a> notifying him of this error.  <strong>Please also include the error report below.  Thank you.</strong>
+                          </p>
+                          <h3>Error Report</h3>
+                          <p>
+<pre><code><?php echo $conn->error; ?></code></pre>
+                          </p>
+                        <?php }
+                      } else { ?>
+                        <h2>Action Denied</h2>
+                        <p>
+                          Sorry, you may not promote anyone who is already a Super Moderator or above.  This is to prevent any accidental promotions of Super Moderators to Administrators which then cannot be undone.
+                        </p>
+                        <p>
+                          If you would like to promote a Super Moderator to Administrator rank, please contact the <a href="mailto:dleung@connect.kellettschool.com">owner of the website</a>.  Upon confirmation, the owner of this website/Service will manually promote said Super Moderator.
+                        </p>
+                        <p>
+                          <a href="index.php" class="button rounded">Return</a>
+                        </p>
+                      <?php }
+                    } else { ?>
+                      <h2>Forbidden</h2>
+                      <p>
+                        Sorry, you are not allowed to execute any action on someone with a higher rank or the same rank as you.
+                      </p>
+                      <p>
+                        <a href="index.php" class="button rounded">Return</a>
+                      </p>
+                    <?php }
+                  }
+                } else { ?>
+                  <h2>User Not Found</h2>
+                  <p>
+                    Sorry, the user you were trying to promote was not found.
+                  </p>
+                  <p>
+                    <a href="index.php" class="button rounded">Return</a>
+                  </p>
+                <?php }
+              } else { ?>
+                <h2>Unauthorised</h2>
+                <p>
+                  Sorry, you are not allowed to execute this action.
+                </p>
+                <p>
+                  <a href="index.php" class="button rounded">Return</a>
+                </p>
+              <?php }
+            }
+          } else { ?>
+            <h2>Invalid Cookies Detected</h2>
+            <p>
+              Sorry, invalid cookies were detected in your browser.  Please try logging out and try again.
+            </p>
+            <p>
+              <a href="logout.php" class="button rounded">Log Out</a>
+            </p>
+          <?php }
+        } else { ?>
+          <h2>Login Required</h2>
+          <p>
+            You must be logged in in order to execute this action.
+          </p>
+          <p>
+            <a href="login.php" class="button rounded">Log In</a>
+          </p>
+        <?php } ?>
+        <hr />
+        <?php require 'footer.php'; ?>
+      </div>
+    </div>
+  </body>
+</html>
